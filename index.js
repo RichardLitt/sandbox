7f609a577fb9b3be5717cae15d5da1369173baf3
Richard Littauer <richard.littauer@gmail.com>
2015-04-16 16:28:49 +0700

    PouchDB sync

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-var TestMe = require('./requireMe.js')({'alertOnFail': false})
+var PouchDB = require('pouchdb')
+var db = new PouchDB('sandbox')
+
+db.put({'_id': 'kitten'})
+
+function saveSelection (documentId, selection) {
+  console.log('Inside saveSelection')
+
+  db.get('kitten').then(function (value) {
+    console.log('value', value)
+
+    /* Instantiate the object if it doesn't exist yet */
+    value = value || {}
+    value._id = 'kitten'
+
+    /* Add in the selection to the selections array */
+    value.selections = value.selections || []
+    value.selections.push(selection)
+
+    /* Get rid of prototypes so we can put this to the database */
+    value = JSON.parse(JSON.stringify(value))
+
+    console.log('value', value)
+    return db.put(value)
+  }).then(function (response) {
+    console.log('response', response)
+
+  }).catch(function (err) {
+    console.log(err)
+  })
+}
+
+saveSelection('kitten', {'tree': 'tree'})
