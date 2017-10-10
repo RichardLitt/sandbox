09e6f899a824dc351c42a15b76a27aab60ca14eb
Richard Littauer <richard.littauer@gmail.com>
2017-10-10 10:12:08 -0400

    Implement random function
    Closes #21

--- coffee.js
+++ coffee.js
@@ -1,1 +1,15 @@
-console.log('hellow world!')
+const coffees = require('./coffee.json')
+
+
+function pickRandomProperty (obj) {
+  var result
+  var count = 0
+  for (var prop in obj) {
+    if (Math.random() < 1 / ++count) {
+      result = prop
+    }
+  }
+  return result
+}
+
+console.log(pickRandomProperty(coffees))
